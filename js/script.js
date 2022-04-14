6d1758c2626a3088edb01faea37922f81a7551bf
William <william.teletabs@hotmail.com>
2022-04-14 17:27:29 -0300

    Criando código para checar elementos selecionados

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
+//ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
-
 function ativarLink(link){
-  
-const url = window.location.href;
-const href = link.href;
+  const url = window.location.href;
+  const href = link.href;
 
-console.log(url);
-console.log(href);
-if(url.includes(href)){
-  link.classList.add("ativo");
+  if(url.includes(href)){
+    link.classList.add("ativo");
   }
 }
 
 links.forEach(ativarLink);
+
+//Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro);
+  if(elemento){
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
